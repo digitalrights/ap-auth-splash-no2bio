3c167eb164a925004231c1cad49293a845782103
Yehuda Deutsch <yeh@uda.co.il>
2014-07-27 14:02:38 +0300

    made all files local, added license

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ ap-auth-splash-no2bio
 =====================
 
 Occupy WiFi - no2bio
+
+License
+-------
+
+Unless specified otherwise, all files are shared under the license in the [LICENSE](LICENSE) file
+
+* css/bootstrap.min.css, bootstrap-theme.min.css, fonts/ and js/ are from [Twitter Inc.](https://github.com/twbs/bootstrap) and shared under the [MIT License](https://github.com/twbs/bootstrap/blob/master/LICENSE).
+* css/bootstrap-rtl.min.css is from [morteza](https://github.com/morteza/bootstrap-rtl) and shared under the [CC by-nc-sa License](https://github.com/morteza/bootstrap-rtl/blob/master/LICENSE.md).
--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <title>אומרים לא למאגר הביומטרי</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.2.0-rc2/css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.min.css">
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
@@ -18,8 +18,7 @@
       <section>
         <div>
           <p>
-            אזרחי ישראל,<br>
-            אל תצטרפו למאגר הביומטרי!
+            אזרחי ישראל, אל תצטרפו למאגר הביומטרי!
           </p>
           <p>
             היזהרו! בביקורכם מיד במשרד הפנים יבקשו מכם למסור את טביעת האצבע בתמורה לתעודת זהות חכמה, במטרה להכניס אתכם למאגר הביומטרי, אשר יסכן אתכם ואת בטחונכם.<br>
@@ -49,7 +48,7 @@
             <input type="hidden" name="gw_port" value="<?php echo $get['gw_port']; ?>">
             <?php endif; ?>
             <input type="hidden" id="authenticators" name="authenticator" value="apAuthSplashOnly">
-            <input type="submit" class="btn btn-success" name="submit[apAuthSplashOnlyConnect]" value="להמשך גלישה">
+            <input type="submit" class="btn btn-success" name="submit[apAuthSplashOnlyConnect]" value="להמשך גלישה חינם">
           </form>
         </div>
       </section>
@@ -57,6 +56,6 @@
       </footer>
     </div>
 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
